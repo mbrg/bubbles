8612fefdc08adbf7ead7c4ef1f7b4f88e255abab
mibarg <11074433+mibarg@users.noreply.github.com>
2021-06-06 20:12:02 +0300

    add print utils

--- Bubble.gd
+++ Bubble.gd
@@ -7,14 +7,12 @@ var torque = 0 #20000
 export var pseudo_gravity_scale = 9800
 export var pseudo_gravity_vec = Vector2(0, -1)
 
-func _ready():
-	disable()
-	enable_if_main()
+export var enable_on_start = false
 
-func enable_if_main():
-	if get_tree().current_scene.name != name:
-		return
-	enable()
+func _ready():
+	hide()
+	if enable_on_start or get_tree().current_scene.name == name:
+		enable()
 
 func set_scale(scale):
 	$Sprite.scale *= scale
@@ -22,24 +20,27 @@ func set_scale(scale):
 	$VisibilityNotifier2D.scale *= scale
 
 func enable():
-	print("Bubble(position=%s,mass=%d)" % [str(position), mass])
-
+	utils_printf("position=%s, mass=%d", [str(position), mass])
+	
 	show()
 	$CollisionShape2D.disabled = false
 	
 	# apply gravity
 	add_force(Vector2(), pseudo_gravity_scale * mass * pseudo_gravity_vec)
 
-func disable():
-	hide()
-	$CollisionShape2D.set_deferred("disabled", true)
-
 func _on_VisibilityNotifier2D_screen_exited():
+	utils_printf("Left screen")
 	queue_free()
 
 func _on_Bubble_body_entered(body):
-	print("POP")
-	disable()
+	utils_printf("POP")
+	hide()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func utils_printf(msg: String, vars: Array = []):
+	var prefix = "[%d] [%s %d] " % [OS.get_unix_time(), name, get_instance_id()]
+	var content = msg % vars
+	print(prefix + content)
 
 # apply force
 func _integrate_forces(state):
