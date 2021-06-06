21ae7ad3e04c45e837e8b11bfb590e81f740e292
mibarg <11074433+mibarg@users.noreply.github.com>
2021-06-06 20:54:59 +0300

    apply constant force and respond to player changes

--- Player.gd
+++ Player.gd
@@ -1,28 +1,33 @@
 extends Node
 class_name Player
 
-signal apply_uniform_impulse(impulse_vec)
+signal apply_uniform_force(force_vec)
 
 export var force_scale = 500
 
+var applied_force = Vector2()
+
 func _physics_process(delta):
-	var applied_impulse = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		applied_impulse.x += 1
-	if Input.is_action_pressed("ui_left"):
-		applied_impulse.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		applied_impulse.y += 1
-	if Input.is_action_pressed("ui_up"):
-		applied_impulse.y -= 1
+	# TODO handle delta
 	
-	if applied_impulse.length() > 0:
-		applied_impulse = applied_impulse.normalized()
-		applied_impulse *= force_scale
-		
-		utils_printf("applying impulse: %s" % [str(applied_impulse)])
-		emit_signal("apply_impulse", applied_impulse)
-		# TODO handle delta
+	var new_applied_force = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		new_applied_force.x += 1
+	if Input.is_action_pressed("ui_left"):
+		new_applied_force.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		new_applied_force.y += 1
+	if Input.is_action_pressed("ui_up"):
+		new_applied_force.y -= 1
+	
+	if new_applied_force.length() > 0:
+		new_applied_force = new_applied_force.normalized()
+		new_applied_force *= force_scale
+	
+	if new_applied_force.distance_to(applied_force) > 0:
+		utils_printf("applying new force: %s" % [str(new_applied_force - applied_force)])
+		emit_signal("apply_force", new_applied_force)
+		applied_force = new_applied_force
 
 func utils_printf(msg: String, vars: Array = []):
 	var prefix = "[%d] [%s %d] " % [OS.get_unix_time(), name, get_instance_id()]
